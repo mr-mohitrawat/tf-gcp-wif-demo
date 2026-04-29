c885ee207588d95015063b0a8361204d44ad4391
Mohit Rawat <67528908+mr-mohitrawat@users.noreply.github.com>
2026-04-29 15:02:28 +0530

    Refactor Google Storage bucket resource configuration
    Updated Google Cloud Storage bucket configuration with variables and added versioning and lifecycle rules.

--- main.tf
+++ main.tf
@@ -1,6 +1,25 @@
-resource "google_storage_bucket" "my-bucket" {
-  name = "bkt-demo-01"
-  location = "us-central1"
-  project = "terraform-494805"
-  public_access_prevention = "enforced"
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "bucket" {
+  name          = var.bucket_name
+  location      = var.location
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 30
+    }
+    action {
+      type = "Delete"
+    }
+  }
 }
